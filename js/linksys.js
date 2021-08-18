fe5155b5d850276408f286b2883d2c9adb0e85c7
lee-mihye <69783624+lee-mihye@users.noreply.github.com>
2021-08-18 17:53:25 +0900

    안먹은걸로

---
+++ js/linksys.js
@@ -0,0 +1,35 @@
+/// GNB메뉴 링크 연결 JS - linksys.js ///
+
+$(function () { /////// jQB ///////////////////////
+    console.log("로딩완료!");
+
+    //////// 상단 코멘트 클릭시 하단 리뷰로 이동/////////
+    $(".comment").click(function (e) {
+        e.preventDefault();
+        var offset = $(".review_title").offset();
+
+        $("html,body").animate({
+            scrollTop: offset.top
+        }, 600);
+    }); ////////comment click/////////////////
+
+
+     // 1. 특정 위치에서 부터 버튼 나타고, 사라지게..효과는 fade로
+    //  $(window).scroll(function () {
+	// 	if ($(this).scrollTop() > 200) {
+	// 		$('.tbtn').fadeIn(200);
+	// 	} else {
+	// 		$('.tbtn').fadeOut(200);
+	// 	}
+	// });
+
+	// 2. 버튼 클릭하면 원하는 위치로 이동
+	$('.tbtn').click(function (event) {
+		event.preventDefault();
+		$('html, body').animate({ scrollTop: 0 }, 300);
+	});
+
+
+
+}); ////////// jQB ///////////////////////////////
+/////////////////////////////////////////////////
---
+++ js/scroll.js
@@ -0,0 +1,59 @@
+//// 안먹은걸로JS - main.js /////
+
+///// 로드구역 //////////////////////////////
+window.addEventListener("DOMContentLoaded",
+    function () {
+        console.log("로딩완료!자바스크립트");
+
+
+        //////////주소복사 스크립트//////////////
+        function copyToClipboard(val) {
+            var t = document.createElement("textarea");
+            document.body.appendChild(t);
+            t.value = val;
+            t.select();
+            document.execCommand('copy');
+            document.body.removeChild(t);
+        }
+
+        $('#url_copy').click(function () {
+            copyToClipboard('https://www.naver.com/');
+
+            toastr.options = {
+                "preventDuplicates": true,
+                "closeButton": false,
+                "timeOut": 2000,
+                "positionClass": "toast-top-center"
+            }
+            toastr.success("주소가 복사되었습니다");
+        }); //////////주소복사 스크립트//////////////
+
+
+
+
+        /////////////TOP버튼(스크롤 위로 올리면 나타나기)///////////
+        var lastScrollTop = 0,
+            delta = 15;
+
+        $(window).scroll(function () {
+            var scrollTop = $(this).scrollTop()
+            /* 스크롤바 수직 위치를 가져옵니다, 괄호 안에 값(value)이 있을 경우 스크롤바의 수직 위치를 정합니다. */
+            // Math.abs: 주어진 숫자의 절대값을 반환(return)합니다.
+            if (Math.abs(lastScrollTop - scrollTop) <= delta) // 스크롤 값을 받아서 ~
+                return; // ~ 리턴
+
+            if ((scrollTop > lastScrollTop) && (lastScrollTop > 0)) {
+                /* 화면에 나오지 않을 때, top값은 요소가 보이지 않을 정도로 사용해야함 */
+                $(".tbtn").css("top", "0px");
+            } else {
+
+                $(".tbtn").css("top", "-100px");
+            }
+            lastScrollTop = scrollTop;
+        }); /////////////TOP버튼(스크롤 위로 올리면 나타나기)///////////
+
+
+
+    }); /////////////// 로드구역 ///////////////////////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
